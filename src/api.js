f3af511b6c97441a7b3ae5920d139c6981390683
Phet11234 <phettest03@gmail.com>
2017-12-17 10:29:07 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -31,7 +31,7 @@ class LineAPI {
     path: this.config.LINE_HTTP_URL,
     https: true
   }) {
-    options.headers['X-Line-Application'] = 'DESKTOPMAC 10.10.2-YOSEMITE-x64 MAC 4.5.0';
+    options.headers['X-Line-Application'] = 'DESKTOPMAC 10.10.2-YOSEMITE-x64 MAC 5.5.1';
     this.options = options;
     this.connection =
       thrift.createHttpConnection(this.config.LINE_DOMAIN, 443, this.options);
